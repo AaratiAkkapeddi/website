540e805bd7acc5897f745721abb03fb391900802
Aarati Akkapeddi <aarati.akkapeddi@gmail.com>
2015-07-08 17:50:07 -0400

    hamburger scroll color change effect

--- JS/main.js
+++ JS/main.js
@@ -5,10 +5,27 @@ function animateHamburger() {
 
 function slideMenu(){
   $('.navbar').toggleClass('open');
+  if(!$('#hamburger-button').hasClass('white') ||
+    ((!$('.navbar').hasClass('open')) && (sT < 200))){
+    $('#hamburger-button').toggleClass('white');
+  }
   /*animating menu slidedown*/
 }
 $('#hamburger-button').click(function(){
   console.log('trying');
   animateHamburger();
   slideMenu();/*attaching click handler to burger*/
+});
+
+var sT;
+
+$(document).ready (function () {
+    $(window).scroll (function () {
+          sT = $(this).scrollTop();
+            if(sT >= 200){
+                $('#hamburger-button').addClass('white');
+            }else{
+                $('#hamburger-button').removeClass('white');
+            }
+    });
 });
